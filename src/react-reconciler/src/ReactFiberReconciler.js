f2aaa7b8666374b07c9dfa6fe804233c628c9334
pengfei.zuo <pengfei.zuo@retech.work>
2023-01-07 18:15:15 +0800

    feat: 开始准备构建Fiber

--- src/react-reconciler/src/ReactFiberReconciler.js
+++ src/react-reconciler/src/ReactFiberReconciler.js
@@ -1,5 +1,6 @@
 import { createFiberRoot } from './ReactFiberRoot';
 import { createUpdate, enqueueUpdate } from './ReactFiberClassUpdateQueue';
+import { scheduleUpdateOnFiber } from './ReactFiberWorkLoop';
 
 export function createContainer(containerInfo) {
   return createFiberRoot(containerInfo);
@@ -20,4 +21,6 @@ export function updateContainer(element, container) {
   //把此更新对象添加到current这个根Fiber的更新队列上,返回根节点
   const root = enqueueUpdate(current, update);
   console.log('root=>', root);
+  //在Fiber上调度更新
+  scheduleUpdateOnFiber(root);
 }
